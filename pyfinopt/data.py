92a33e0491e234c13103e562aec380eced50ec0a
danyanyam <dvbuchko@edu.hse.ru>
2022-03-21 17:50:11 +0300

    final remarks

--- pyfinopt/data.py
+++ pyfinopt/data.py
@@ -1,17 +1,29 @@
-from dataclasses import dataclass, field
-from typing import Set, Dict, Iterable
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+from dataclasses import dataclass, field
+from typing import Set, Dict, Iterable, List, Optional
 
 
 @dataclass
 class Stock:
+    """ Object, that represents stock data model """
+
     ticker: str = None
     returns: np.array = None
 
-    def merge(self, rhs):
+    def merge(self, rhs) -> None:
+        """ Merges two stocks in one, by concatting their returns
+
+        Arguments:
+            rhs :: Stock,
+                another stock to merge
+        """
+
         assert rhs.ticker == self.ticker
         self.returns = np.vstack(self.returns, rhs.returns)
+
+    def __len__(self):
+        return len(self.returns)
 
     def __repr__(self):
         return f'Stock(ticker={self.ticker}, data_len={len(self.returns)})'
@@ -19,6 +31,13 @@ class Stock:
 
 @dataclass
 class Portfolio:
+    """Object that represents collection of stocks. The critical
+    assumption of the portfolio is that
+
+
+    Returns:
+        _type_: _description_
+    """
     stocks: Dict[str, Stock] = field(default_factory=dict)
     weights: Dict[str, float] = field(default_factory=dict)
     _params: np.array = None
@@ -33,8 +52,22 @@ class Portfolio:
 
     @staticmethod
     def from_pandas(stocks, ticker_col: str, returns_col: str):
+        """Creates portfolio object from pandas dataframe. Critical assumption:
+        all of the stocks have the same date range and same length.
+
+        Arguments:
+            stocks :: pd.DataFrame
+                data with stocks
+            ticker_col :: str
+                column name of column with ticker names
+            returns_col :: str
+                columns name of column with returns
+
+        Returns:
+            Portfolio
+        """
         _stocks = {}
-        stocks_it = stocks.groupby(ticker_col)
+        stocks_it = stocks.dropna().groupby(ticker_col)
 
         for ticker, stock in stocks_it:
 
@@ -48,24 +81,111 @@ class Portfolio:
             existing_stock.merge(s)
             _stocks.update({ticker: existing_stock})
 
+        lengths = set(len(stock) for stock in _stocks.values())
+
+        if len(lengths) != 1:
+            raise Exception('Cant create portfolio from '
+                            f'different returns sizes: {lengths}')
+
         return Portfolio(_stocks)
 
-    def get_stocks(self, tickers: Iterable[str]):
+    def add_stock(self, stock: Stock) -> None:
+        """Adds stock to the portfolio
+
+        Args:
+            stock :: Stock
+                stock to add to the portfolio
+
+        Raises:
+            Exception:
+                in case stock is not np.array of size of other stocks
+
+        """
+        assert isinstance(stock.returns, np.array), 'Only np arrays are allowed'
+        assert len(stock.returns.shape) == 2, 'Only 2-dimensional return vectors allowed'
+        assert stock.returns.shape[-1] == 1, 'Only 2-dimensional vector-columns are allowed'
+
+        if len(self.tickers) == 0:
+            self.stocks[stock.ticker] = stock
+            return
+
+        some_stock = self.stocks[self.tickers[0]]
+        if len(stock) != len(some_stock):
+            raise Exception('Cant add stock due to the size mismatch '
+                            f'allowed returns shape is {len(some_stock)} '
+                            f'but received: {len(stock)}')
+
+        existing_stock = self.stocks.get(stock.ticker)
+
+        if existing_stock != None:
+            existing_stock.merge(stock)
+            stock = existing_stock
+
+        self.stocks[stock.ticker] = stock
+
+    def get_stocks(self, tickers: Iterable[str]) -> List[Stock]:
+        """Returns list of Stock objects within the portfolio
+
+        Args:
+            tickers :: Iterable[str],
+                Iterable with tickers to select stocks by
+
+        Returns:
+            List[Stock],
+                List of selected stocks
+        """
         return [self.get_stock(ticker) for ticker in tickers]
 
-    def get_returns(self, tickers: Iterable[str] = None):
+    def get_returns(self, tickers: Optional[Iterable[str]] = None) -> np.array:
+        """Returns matrix of returns for all stocks
+
+        Args:
+            tickers :: Iterable[str], optional
+                list of tickers to select stocks from. If empty, then all returns are returned
+
+        Returns:
+            np.array,
+                returns of all stocks
+
+        """
         if tickers:
             return np.hstack([stock.returns for stock in self.get_stocks(tickers)])
         return np.hstack([stock.returns for stock in self.stocks.values()])
 
-    def get_stock(self, ticker: str):
+    def get_stock(self, ticker: str) -> Stock:
+        """Returns stock from portfolio by ticker
+
+        Args:
+            ticker :: str,
+                ticker of stock in the portfolio to return
+
+        Returns:
+            Stock
+        """
         return self.__getitem__(ticker)
 
-    def cov(self, tickers: Iterable[str] = None, annualized=True):
+    def cov(self, tickers: Optional[Iterable[str]] = None, annualized: bool = True) -> np.array:
+        """Calculates the annualized covariance of columns of returns of
+        specified tickers. If no tickers specified, then covariance of all returns
+        in portfolio are calculated.
+
+        Args:
+            tickers :: Iterable[str], optional
+                list of tickers to select stocks from. If empty, then all returns are returned
+            annualized :: bool, optional
+                whether to calculate annualized returns or not
+
+        Returns:
+            np.array: covariance matrix
+        """
         multiplicator = 253 if annualized else 1
         return np.cov(self.get_returns(tickers), rowvar=0) * multiplicator
 
-    def plot(self):
+    def plot(self) -> None:
+        """Plots cumulated return of stocks in the portfolio.
+        If portfolio have been optimized, then portfolio performance
+        is also shown.
+        """
         _, ax = plt.subplots(figsize=(16, 5), dpi=200)
         ax.plot(
             np.cumprod(1 + self.get_returns(), axis=0),
@@ -76,7 +196,7 @@ class Portfolio:
         ax.axhline(1, color='grey', linestyle='dashed')
         ax.grid(alpha=0.2)
 
-        if self.weights is not None:
+        if self.is_optimized:
             ax.plot(
                 np.cumprod(1 + self.get_returns() @ self._params, axis=0),
                 linewidth=1.5,
@@ -85,18 +205,27 @@ class Portfolio:
         ax.set_title('Cumulative return of stocks in Portfolio')
         ax.legend()
 
-    def subset(self, tickers):
+    def subset(self, tickers: Iterable[str]):
+        """Copies portfolio with specified tickers """
         return Portfolio(dict(zip(tickers, self.get_stocks(tickers))))
 
-    def set_weights(self, weights: Dict[str, float]):
-        assert set(self.tickers) == set(weights)
+    def set_weights(self, weights: Dict[str, float]) -> None:
+        """Sets weights of stocks in the portfolio if weights correspond
+        to the stocks inside portfolio
+
+        Args:
+            weights :: Dict[str, float]
+                weights of stocks inside the portfolio
+        """
+        assert set(self.tickers) == set(weights), 'Sets of tickers dont correspond'
         self.weights = weights
         self._params = np.array(list(weights.values()))
 
     def __iter__(self):
+        """ to iterate over portfolio """
         return iter(self.stocks.items())
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: str) -> Stock:
         try:
             assert key in self.tickers
         except AssertionError:
@@ -105,7 +234,7 @@ class Portfolio:
 
         return self.stocks[key]
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.stocks)
 
     def __repr__(self) -> str:
--- pyfinopt/optimizers.py
+++ pyfinopt/optimizers.py
@@ -1,65 +1,109 @@
 import numpy as np
 import scipy.optimize as sco
+from typing import Tuple
+from abc import ABCMeta, abstractmethod
 
 from .data import Portfolio
 
 
-class BaseOptimizer:
+class BaseOptimizer(metaclass=ABCMeta):
+    """ Base class for optimizers """
+
     def __init__(self, portfolio: Portfolio):
-        self._init_weights = np.random.random(size=len(portfolio)).reshape(-1, 1)
+        self._init_weights = np.random.random(size=len(portfolio)).reshape(-1, 1)  # k x 1
         self._portfolio = portfolio
         self._normalized = 1 / len(self._init_weights) * np.ones(self._init_weights.shape)
-        self._weights = {}
+        self.weights = {}
+        self.constraints = [{'type': 'eq', 'fun': lambda weights: np.sum(weights) - 1}]
         self.params = None
+        self._optim = None
 
-    def target(self):
-        pass
+    @abstractmethod
+    def target(self, weights: np.array) -> float:
+        """ Must define the function to be optimized """
 
-    def constraints(self):
-        return [{'type': 'eq', 'fun': lambda weights: np.sum(weights) - 1}]
-
-    def bounds(self, short_possible: bool = False):
+    def _bounds(self, short_possible: bool = False) -> Tuple:
+        """ Bounds on weights """
         w_range = (-1, 1) if short_possible else (0, 1)
         return tuple(w_range for _ in range(len(self._portfolio)))
 
-    def minimize(self, short_allowed: bool = False):
+    def _minimize(self, short_allowed: bool = False):
         return sco.minimize(
             fun=self.target,
             x0=self._normalized,
             method='SLSQP',
-            bounds=self.bounds(short_allowed),
-            constraints=self.constraints()
+            bounds=self._bounds(short_allowed),
+            constraints=self.constraints
         )
+
+    def weighted_return(self, weights: np.array) -> np.array:
+        return (self._portfolio.get_returns().mean(axis=0) * 253) @ weights
+
+    def weighted_covariance_return(self, weights: np.array) -> np.array:
+        return weights.T @ self._portfolio.cov(annualized=True)  @ weights
 
     def optimize(self, short_allowed: bool = False):
         self._params = None
-        results = self.minimize(short_allowed)
-        self._params = results['x']
+        self._optim = self._minimize(short_allowed)
+        self._params = self._optim['x']
         self.weights = dict(zip(self._portfolio.tickers, self._params))
+
+    def __repr__(self):
+        cls_name = self.__class__.__name__
+
+        if self._optim:
+            return f'{cls_name}(optimized={True}, value={self._optim["fun"]})'
+        return f'{cls_name}(optimized={False})'
 
 
 class VolatilityOptimizer(BaseOptimizer):
+    """ minimizes volatility """
 
-    def target(self, weights):
-        return np.sqrt(weights.T @ self._portfolio.cov(annualized=True)  @ weights)
+    def target(self, weights: np.array) -> float:
+        return np.sqrt(self.weighted_covariance_return(weights))
+
+
+class ReturnOptimizer(BaseOptimizer):
+    """ maximizes return """
+
+    def target(self, weights: np.array) -> float:
+        return self.weighted_return(weights)
 
 
 class SharpeOptimizer(BaseOptimizer):
+    """ maximizes sharpe ratio """
 
-    def target(self, weights):
-        ann_returns = self._portfolio.get_returns().mean(axis=0) * 253
-        portfolio_returns = ann_returns @ weights
-        portfolio_std = np.sqrt(weights.T @ self._portfolio.cov(annualized=True) @ weights)
-        return -1 * (portfolio_returns / portfolio_std)
-
-
-class EfficientRiskOptimizer(BaseOptimizer):
-    pass
+    def target(self, weights: np.array) -> float:
+        return_ = self.weighted_return(weights)
+        std = np.sqrt(self.weighted_covariance_return(weights))
+        return -1 * (return_ / std)
 
 
-class EfficientReturnOptimizer(BaseOptimizer):
-    pass
+class EfficientRiskOptimizer(VolatilityOptimizer):
+    """ minimizes risk given target return """
+
+    def __init__(self, target_return: float, portfolio: Portfolio):
+        super().__init__(portfolio)
+        self.constraints.append(
+            {'type': 'eq', 'fun': lambda w: self.weighted_return(w) - target_return}
+        )
+
+
+class EfficientReturnOptimizer(ReturnOptimizer):
+    """ maximizes return given target risk """
+
+    def __init__(self, target_risk: float, portfolio: Portfolio):
+        super().__init__(portfolio)
+        self.constraints.append(
+            {'type': 'eq',
+             'fun': lambda w: np.sqrt(self.weighted_covariance_return(w)) - target_risk}
+        )
 
 
 class MaximumDiversificationOptimizer(BaseOptimizer):
-    pass
+    """ maximizes diversification """
+
+    def target(self, weights: np.array) -> float:
+        weighted_volatilities = weights.T @ self._portfolio.get_returns().std(axis=0)
+        std = np.sqrt(self.weighted_covariance_return(weights))
+        return -1 * (weighted_volatilities / std)
